c823f9aa71b69b1b2c8cf541b0c361ff6a9ad07e
Jumner <justin462@protonmail.com>
2025-10-21 20:36:16 -0400

    Added blocklist to clear out junk

--- install/common/lightpad.sh
+++ install/common/lightpad.sh
@@ -8,6 +8,7 @@ sudo apt install -y $MODULE_ROOT/bin/lightpad.deb
 # Link config
 mkdir -p "$HOME/.lightpad"
 ln -s "/opt/car-pi/modules/ui-module/config/lightpad/config" "$HOME/.lightpad/config"
+ln -s "/opt/car-pi/modules/ui-module/config/lightpad/blocklist" "$HOME/.lightpad/blocklist"
 
 # Set wallpaper to match current
 ln -s "$(realpath ~/.local/state/wpaperd/wallpapers)/$(ls ~/.local/state/wpaperd/wallpapers)" "$(realpath ~/.lightpad/background.jpg)"
